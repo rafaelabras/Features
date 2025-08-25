65e0ca6f68e87fdb2315d36a69d7e35d038c0ead
Rafael Abras <rafaelabraslessa@gmail.com>
2025-08-25 14:40:13 -0300

    feat: Stepwise Builder

---
+++ Design Patterns/Builder/Builder/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.ComponentModel.Design.Serialization;
+using System.Text;
+
+namespace Builder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //var builder = new HtmlBuilder("ul");
+            //builder.addChild("li", "hello!");
+            //builder.addChild("li", "world!");
+            //Console.WriteLine(builder.ToString());
+
+
+        }
+        
+    }
+
+    public class HtmlElement
+    {
+        private const int identS = 2;
+        public string Nome, Texto;
+        public List<HtmlElement> Elementos = new List<HtmlElement>();
+
+        public HtmlElement()
+        {
+        }
+
+        public HtmlElement(string name, string text)
+        {
+            Nome = name;
+            Texto = text;
+        }
+
+        private string ToStringImplementation(int ident)
+        {
+            var sb = new StringBuilder();
+            var i = new string(' ', identS * ident);
+            sb.AppendLine($"{i}<{Nome}> ");
+
+            if (!string.IsNullOrEmpty(Texto))
+            {
+                sb.Append(new string(' ', identS * (ident + 1)));
+                sb.AppendLine(Texto);
+            }
+
+            foreach (var el in Elementos)
+            {
+                sb.Append(el.ToStringImplementation(ident + 1));
+            }
+
+            sb.AppendLine($"{i}</{Nome}> ");
+
+            return sb.ToString();
+        }
+
+
+        public override string ToString()
+        {
+            return ToStringImplementation(0);
+        }
+    }
+    public class HtmlBuilder()
+    {
+        private readonly string elNome;
+        HtmlElement el = new HtmlElement();
+            
+
+        public HtmlBuilder(string elNome) : this()
+        {
+            this.elNome = elNome;
+            el.Nome = elNome;
+        }
+
+        public void addChild(string childName, string childText)
+        {
+            var e = new HtmlElement(childName, childText);
+            el.Elementos.Add(e);
+        }
+
+        public override string ToString()
+        {
+            return el.ToString();
+        }
+
+        public void Clear()
+        {
+            el = new HtmlElement{Nome = elNome};
+        }
+    }
+}
+
---
+++ Design Patterns/Builder/StepWise Builder/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.ComponentModel.Design.Serialization;
+using System.Data;
+using System.Text;
+
+namespace Builder
+{
+    public enum CarType
+    {
+        Sedan,
+        Crossover
+    }
+    
+    public class Car
+    {
+        public CarType Type;
+        public int WheelSize;
+        
+    }
+
+    public interface ISpecifyCarType
+    {
+        ISpecifyWheelSize OfType(CarType type);
+    }
+
+    public interface ISpecifyWheelSize
+    {
+        IBuildCar WithWheels(int size);
+    }
+
+    public interface IBuildCar
+    {
+        public Car Build();
+    }
+
+    public class CarBuilder()
+    {
+        private class Impl :
+            ISpecifyCarType, 
+            ISpecifyWheelSize, 
+            IBuildCar
+        {
+            private  Car car = new Car();
+            
+            public Tuple<String, String> cargetter(string cartype, string wheelsize)
+            {
+                return new Tuple<string, string>(car.Type.ToString(), car.WheelSize.ToString());
+            }
+            public ISpecifyWheelSize OfType(CarType type)
+            {
+                car.Type = type;
+                return this;
+            }
+            
+            public IBuildCar WithWheels(int size)
+            {
+                switch (car.Type)
+                {
+                    case CarType.Crossover when size < 17 || size > 20:
+                    case CarType.Sedan when size < 15 || size > 17:
+                        throw new ArgumentException($"O tamanho das rodas estão errads para o {car.Type}");
+                }
+
+                car.WheelSize = size;
+                return this;
+            }
+
+            public Car Build()
+            {
+                Console.WriteLine( cargetter(car.Type.ToString(), car.WheelSize.ToString()));
+                return car;
+            }
+            
+            
+        }
+        
+        public static ISpecifyCarType create()
+        {
+            return  new Impl();
+        }
+        
+        
+    }
+    
+    public class StepwiseBuilder
+    {
+        static void Main(string[] args)
+        {
+         var car = CarBuilder.create()
+             .OfType(CarType.Crossover)
+             .WithWheels(18)
+             .Build();
+
+        }
+        
+    }
+}
+
+    
